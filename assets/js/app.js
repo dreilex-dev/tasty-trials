04e62a76a050ae2efe8097b33171fc3437daf36f
Andrei Boaca <dreilex@Andreis-MacBook-Pro.local>
2024-12-12 13:49:46 +0100

    Event listener added on form with submit event

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,32 @@
 const mealInput = document.getElementById('searchInput');
 const feedbackMessage = document.getElementById('feedbackMessage');
 const containerResults = document.getElementById('resultsRow'); 
-const searchButton = document.getElementById('searchButton');
+const searchForm = document.getElementById('searchForm');
 const resultMessage = document.getElementById('resultMessage');
 const resultAlert = document.getElementById('result-alert');
-const cardTitleElement = document.querySelector('.card-title');
-// cardTitleElement.classList.add('styleCardH5');
 
-// Funcție pentru afișarea mesajelor (verde pentru succes, roșu pentru eroare)
 const showResultMessage = (message, type) => {
     resultAlert.textContent = message;
 
-    // Eliminăm clasele anterioare
     resultMessage.classList.remove('bg-success', 'bg-danger', 'd-none', 'show');
 
-    // Adăugăm clasa corespunzătoare
     if (type === "success") {
         resultMessage.classList.add('bg-success');
     } else if (type === "error") {
         resultMessage.classList.add('bg-danger');
     }
 
-    // Afișăm mesajul
     resultMessage.classList.add('show');
 
-    // Ascundem mesajul după 3 secunde
     setTimeout(() => {
         resultMessage.classList.remove('show');
         resultMessage.classList.add('d-none');
     }, 3000);
 };
 
-// Event listener pentru butonul de căutare
-searchButton.addEventListener("click", function () {
+
+searchForm.addEventListener("submit", function (e) {
+    e.preventDefault(); 
     const mealName = mealInput.value.trim();
     if (mealName) {
         fetchData(mealName);
@@ -42,23 +36,6 @@ searchButton.addEventListener("click", function () {
     }
 });
 
-// Event listener pentru Enter în câmpul de căutare
-mealInput.addEventListener("keydown", function (e) {
-    resultMessage.classList.add('d-none'); // Ascundem mesajul anterior
-
-    if (e.code === "Enter") {
-        e.preventDefault();
-        const mealName = mealInput.value.trim();
-        if (mealName) {
-            fetchData(mealName);
-            mealInput.value = "";
-        } else {
-            showResultMessage("Please enter the meal.", "error");
-        }
-    }
-});
-
-// Funcție pentru fetch API
 const fetchData = async (mealName) => {
     try {
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
@@ -67,17 +44,15 @@ const fetchData = async (mealName) => {
         }
         const data = await res.json();
 
-        containerResults.innerHTML = ""; // Resetăm containerul pentru rezultate
+        containerResults.innerHTML = ""; 
 
         if (!data.meals) {
             showResultMessage("No meals found.", "error");
             return;
         }
 
-        // Afișăm mesajul de succes
         showResultMessage(`These are the results for "${mealName}".`, "success");
 
-        // Afișăm mesele
         displayMeals(data.meals);
     } catch (error) {
         console.error("Fetch error: ", error);
@@ -85,9 +60,8 @@ const fetchData = async (mealName) => {
     }
 };
 
-// Funcție pentru afișarea rezultatelor
 const displayMeals = (meals) => {
-    containerResults.innerHTML = ""; // Golește containerul înainte de a afișa noile rezultate
+    containerResults.innerHTML = ""; 
 
     meals.forEach((meal) => {
         const card = `
@@ -95,12 +69,11 @@ const displayMeals = (meals) => {
                 <div class="card">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
                     <div class="card-body">
-                        <h5 class="card-title styleCardH5">${meal.strMeal}</h5>
+                        <h5 class="card-title">${meal.strMeal}</h5>
                     </div>
                 </div>
             </div>
         `;
         containerResults.innerHTML += card;
     });
-    // cardTitleElement.classList.add('styleCardH5');
 };
